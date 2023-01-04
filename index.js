f1844fb737809b941ea2a593eca812383e779847
Pradosh <pradoshnathan@gmail.com>
2023-01-04 19:17:33 +0530

    Importing the source

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.isFPAvulnerable = isFPAvulnerable;
+
+function isFPAvulnerable(a, b, decimal_leng) {
+    a *= decimal_leng; b *= decimal_leng;
+    // ^ Removing decimal so division is easy
+
+    if (a % 2 == 0 && b % 2 == 0) { // Even number
+        console.log("Not Vulnerable!")
+        return false
+    } else if (a % 2 != 0 && b % 2 != 0) { // Odd number
+        console.log("Not Vulnerable!")
+        return false
+    } else if (b % a == 0) { // Multiples of it
+        console.log("Not Vulnerable!")
+        return false
+    } else if (a % 2 == 0 && b % 2 != 0) { // a is even; b is odd
+        console.log("Vulnerable!")
+        return true
+    } else if (a % 2 != 0 && b % 2 == 0) { // a is odd; b is even
+        console.log("Vulnerable!")
+        return true
+    } else {
+        console.log("Vulnerable!")
+        return true
+    }
+}
